0b8406f2bf1304d7ce1560dd6c118dfcee19b44e
Ravi Pidatala <rtej@apple.com>
2016-04-02 11:46:27 -0700

    commit

--- src/com/chapter10/Question1.java
+++ src/com/chapter10/Question1.java
@@ -3,11 +3,23 @@ package com.chapter10;
 	Reverse integer
 
 	take integer from last digit and put it into front.
+	Saw answer online.
 */
 public class Question1 {
 
 	public static void main(String[] args) {
+		System.out.println(reverse(-983409));
 		
+	}
+
+	static int reverse(int x) {
+		int rev = 0;
+
+		while (x != 0) {
+			rev = rev * 10 + x % 10;
+			x = x / 10;
+		}
+		return rev;
 	}
 
 }
