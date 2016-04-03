244a62b4ac4f549d6698306d57fc2772c4ed9b9b
Ravi Pidatala <rtej@apple.com>
2016-04-03 14:46:25 -0700

    fixing issue

---
+++ src/com/chapter10/Question2.java
@@ -0,0 +1,27 @@
+package com.chapter10;
+//Determine whether an integer is a palindrome. Do this without extra space.
+
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	/*
+		if reverse of number = number, then it is pallindrome.
+		do reverse of the number. Check if reverse is equal to the number . 
+		Convert integer to string. 
+	*/
+
+//	static boolean isPallindrome(int n) {
+//		
+//	}
+
+}
